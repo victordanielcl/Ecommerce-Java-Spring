0281dbedd770ce8f1603cfcc2c80b858f5e8792a
Daniel <victordaniel0101@gmail.com>
2024-06-29 20:26:50 -0600

    Inicializando proyecto

---
+++ src/main/java/com/ecommerce/ecommerce_java_spring/EcommerceJavaSpringApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.ecommerce_java_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceJavaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceJavaSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecommerce/ecommerce_java_spring/EcommerceJavaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.ecommerce_java_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceJavaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
